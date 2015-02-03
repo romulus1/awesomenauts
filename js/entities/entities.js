a84202d0426b39c46cfabf695a5f7c437caac5ba
Luis Cedano <king.romanus1@gmail.com>
2015-02-02 18:59:59 -0800

    Finally got the tower to work

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,20 +11,109 @@ game.PlayerEntity = me.Entity.extend({
             }
         }]);
     
-        this.body.setVelocity(5,0);
-        
+        this.body.setVelocity(5,20);
+             
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+    
+        this.renderable.setCurrentAnimation("idle");
     },
     
     update: function(delta){
         if(me.input.isKeyPressed("right")){
+            //
+            //
+            //
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-        }
-        else{
+            this.flipX(true);
+        }else{
             this.body.vel.x = 0;
         }
         
+        if(!this.body.vel.x !== 0){
+            if(!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+        } else {
+            this.renderable.setCurrentAnimation("idle");
+        }
+
+        
         this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
         return true;
+    }
+});
+
+game.PlayerBaseEntity = me.Entity.extend ({
+    init : function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+             image: "tower",
+             width: 100,
+             height: 100,
+             spritewidth: "100",
+             spriteheight: "100",
+             getShape: function(){
+                 return (new me.Rect(0, 0, 100, 100)).toPolygon();
+             }
+        }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "PlayerBaseEntity";
+    },    
+        
+    update:function(delta) {
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
         
     }
+    
+});
+
+game.EnemyBaseEntity = me.Entity.extend ({
+    init : function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+             image: "tower",
+             width: 100,
+             height: 100,
+             spritewidth: "100",
+             spriteheight: "100",
+             getShape: function(){
+                 return (new me.Rect(0, 0, 100, 100)).toPolygon();
+             }
+        }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "EnemyBaseEntity";
+    },    
+        
+    update:function(delta) {
+        if(this.health<=0){
+            this.broken = true;
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+    
 });
--- js/resources.js
+++ js/resources.js
@@ -7,6 +7,8 @@ game.resources = [
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "player", type:"image", src: "data/img/orcSpear.png"},
+        {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
+        
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -18,8 +20,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
         
-        {name: "level01", type: "tmx", src: "data/map/test.tmx"},
-        {name: "untitled", type: "tmx", src: "data/map/untitled.tmx"},
+        {name: "level", type: "tmx", src: "data/map/test.tmx"},
+        {name: "level01", type: "tmx", src: "data/map/untitled.tmx"},
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
