e11b323f54f8ed8bf9b36f2852abb7be9cff6fc9
Luis Cedano <king.romanus1@gmail.com>
2015-02-05 19:24:33 -0800

    Vid 16
    Includes block from above

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -93,7 +93,11 @@ game.PlayerEntity = me.Entity.extend({
             
             console.log("xdif " + xdif + " ydif " + ydif);
             
-            if(xdif>-35 && this.facing==='right' && xdif<0){ //
+            if(ydif<-40){
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }
+            else if(xdif>-35 && this.facing==='right' && xdif<0){ //
                 this.body.vel.x = 0; //
                 this.pos.x = this.pos.x-1; //
             } else if(xdif<70 && this.facing==='left' && xdif>0){
